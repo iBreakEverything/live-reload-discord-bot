9577577b6193bd7f33a33456c3d5742838c545ac
Gigel <1362764+iBreakEverything@users.noreply.github.com>
2022-01-25 01:27:40 +0200

    Update purge.js
    now 2 week old and older messages can be deleted by purge

--- commands/purge.js
+++ commands/purge.js
@@ -1,35 +1,47 @@
-import { Logger } from "../util/logger.js";
+import { Logger } from '../util/logger.js';
 
 const lowPurgeCount = 1;
-const highPurgeCount = 500;
+const highPurgeCount = 100;
 
 export const purge = {
-    description: 'Deletes given ammount of messages',
+    description: 'Deletes given ammount of messages\nCan delete older messages using 2nd argument',
     name: 'purge',
     aliases: [ 'prune' ],
-    usage: '<count>',
+    usage: '<count> <true?>',
     cooldown: 5,
     permissionRequired: 3,
     reqArgs: true,
     guildOnly: true,
+    /**
+     * 
+     * @param {import('discord.js').Client} client 
+     * @param {import('discord.js').Message} msg 
+     * @param {*} args 
+     * @returns 
+     */
     async execute(client, msg, args) {
-        let count = parseInt(args[0]);
+        const count = parseInt(args[0]);
+        const deleteOld = args[1] === 'true' ? true : false;
         if (isNaN(count)) {
             return Logger.userLog(msg, Logger.TYPE.ERR, 'Count must be a number');
         } else if (count < lowPurgeCount || count > highPurgeCount) {
             return Logger.userLog(msg, Logger.TYPE.ERR, `Count must be between ${lowPurgeCount} and ${highPurgeCount}`);
         }
         try {
-            let toDelete = count;
-            await msg.channel.bulkDelete(1, true);
-            while(toDelete > 0) {
-                await msg.channel.bulkDelete(Math.min(100, toDelete), true);
-                toDelete -= 100;
+            if (!deleteOld) {
+                await msg.channel.bulkDelete(1, true);  // Delete command
+                let delCount = await msg.channel.bulkDelete(Math.max(highPurgeCount, count), true);
+                await msg.channel.send(`Deleted ${delCount.size} messages`).then(msg => msg.delete({ timeout:3000 }));
+            } else {
+                await msg.channel.bulkDelete(1, true);  // Delete command
+                let msgs = await msg.channel.messages.fetch({ limit: count });
+                for (let [key, _] of msgs) {
+                    msgs.get(key).delete();  // FIXME add to queue
+                }
+                msg.channel.send(`Deleting ${msgs.size} messages...`).then(msg => msg.delete({ timeout:3000 }));
             }
-            let botMsg = await msg.channel.send(`Deleted ${count} messages`);
-            botMsg.delete({ timeout:3000 });
         } catch (err) {
-            return Logger.dualLog(client, msg, Logger.TYPE.ERR, `Could not delete ${Math.min(0, toDelete)} messages.`, `Failed to delete ${Math.min(0, toDelete)} messages.`, err);
+            return Logger.dualLog(client, msg, Logger.TYPE.ERR, `Could not delete ${count} messages.`, `Failed to delete ${count} messages.`, err);
         }
     },
 };
