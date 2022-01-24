d6415738bdd80ecf80a00c8fe03889927ff2b151
Gigel <1362764+iBreakEverything@users.noreply.github.com>
2022-01-25 01:27:13 +0200

    Update ping.js
    rewrote to update message only once

--- commands/ping.js
+++ commands/ping.js
@@ -7,23 +7,25 @@ export const ping = {
     permissionRequired: 0,
     reqArgs: false,
     guildOnly: false,
-    async execute(client, msg, args) {
-        let botMsg = await msg.channel.send('〽️ Pinging...');
-        botMsg.edit('🏓').then(
-        botMsg.edit({ embed: {
-            title: 'Ping',
-            description: [
-            `**Server**: ${botMsg.createdAt - msg.createdAt}ms`,
-            `**API**: ${Math.round(client.ws.ping)}ms`,
-            `**Uptime**:  ${msToTime(client.uptime)}`
-            ].join('\n'),
-            color: 0x4aa444,
-            timestamp: new Date()
-        }})
-        .catch((err) => {
-            botMsg.delete({ timeout: 3000 });
-            Logger.dualLog(client, msg, Logger.TYPE.ERR, 'Could not send/edit message.', 'Failure on ping message edit.', err);
-        }));
+    async execute(client, msg, _) {
+        msg.channel.send('〽️ Pinging...').then(botMsg => {
+            botMsg.edit({
+                content: '🏓',
+                embed: {
+                    title: 'Ping',
+                    description: [
+                    `**Server**: ${botMsg.createdAt - msg.createdAt}ms`,
+                    `**API**: ${Math.round(client.ws.ping)}ms`,
+                    `**Uptime**:  ${msToTime(client.uptime)}`
+                    ].join('\n'),
+                    color: 0x4aa444,
+                    timestamp: new Date()
+                }
+            }).catch((err) => {
+                botMsg.delete({ timeout:3000 });
+                Logger.dualLog(client, msg, Logger.TYPE.ERR, 'Could not send/edit message.', 'Failure on ping message edit.', err);
+            });
+        });
     },
 };
 
